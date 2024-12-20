23830280d09a2c95f6a4b023d6efd68bdf15cfa4
pdkvi <pdkvi0@gmail.com>
2024-12-21 01:33:28 +0300

    feat(tasks): add more complete support for cmake
    Replaced the old implementation of cmake support using overseer.nvim
    with a new one implemented using the cmake-tools.nvim plugin.

--- lua/overseer/template/cmake/init.lua
+++
@@ -1,6 +0,0 @@
-local modules = { "build", "configure" }
-for i, _ in ipairs(modules) do
-	modules[i] = "cmake." .. modules[i]
-end
-
-return modules
--- lua/plugins/specs/tasks.lua
+++ lua/plugins/specs/tasks.lua
@@ -1,9 +1,27 @@
 return
 {
-	"stevearc/overseer.nvim",
+	"Civitasv/cmake-tools.nvim",
 	config = function()
-		local overseer = require("overseer")
-		overseer.setup({})
-		overseer.load_template("cmake")
+		local cmake = require("cmake-tools")
+		cmake.setup({
+			cmake_regenerate_on_save = false,
+			cmake_dap_configuration = { type = "lldb" }
+		})
+
+		vim.api.nvim_create_autocmd("DirChanged",
+		{
+			callback = function()
+				local cwd = vim.uv.cwd()
+				local file = io.open(vim.fs.normalize(cwd .. "/CMakeLists.txt"), "r")
+				if file == nil then return end
+				file:close()
+
+				local Config = require("cmake-tools.config")
+				local config = Config:new(require("cmake-tools.const"))
+
+				Config.cwd = vim.fn.resolve(cwd)
+				cmake.register_scratch_buffer(config.executor.name, config.runner.name)
+			end
+		})
 	end
 }
