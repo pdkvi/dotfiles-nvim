3bc72b7486abb8cb02ba060ec8d8ad25afa3b3de
pdkvi <pdkvi0@gmail.com>
2024-12-22 04:18:42 +0300

    feat(colorscheme): color palette has been changed
    The color scheme now has green tones and the text has more contrast.

--- lua/plugins/specs/colorscheme.lua
+++ lua/plugins/specs/colorscheme.lua
@@ -3,15 +3,24 @@ return
 	"slugbyte/lackluster.nvim",
 	proprity = 1000,
 	init = function()
+		local color = require("lackluster").color
+		local accent_color = "#5a725a"
+
 		require("lackluster").setup({
 			disable_plugin = {},
-			tweak_syntax = {},
+			tweak_syntax =
+			{
+				string = accent_color,
+				type = color.green,
+				type_primitive = color.green,
+				func_param = color.gray7,
+			},
 			tweak_background = { popup = "none" },
 		})
 
-		vim.cmd.colorscheme("lackluster-dark")
+		vim.cmd.colorscheme("lackluster")
 
-		local color = require("lackluster").color
+		vim.api.nvim_set_hl(0, "@type.definition", { link = "@type", force = true })
 
 		vim.api.nvim_set_hl(0, "WinBar", { fg = color.gray7, bg = color.gray3, bold = true })
 		vim.api.nvim_set_hl(0, "WinBarNC", { fg = color.gray7, bg = color.gray3, bold = false })
