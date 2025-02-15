f9f2a1e8575a46bb131dae1919b5c39a0114dcbd
pdkvi <pdkvi0@gmail.com>
2025-02-15 22:13:55 +0300

    feat(colorscheme): change colorcolumn appearance

--- lua/plugins/specs/colorscheme.lua
+++ lua/plugins/specs/colorscheme.lua
@@ -18,6 +18,9 @@ return
             tweak_background = { popup = "none" },
         })
 
+        local color_special = require("lackluster.color-special")
+        local theme = require("lackluster.create-theme")(color, color_special)
+
         vim.cmd.colorscheme("lackluster")
 
         vim.api.nvim_set_hl(0, "@type.definition", { link = "@type", force = true })
@@ -25,5 +28,7 @@ return
         vim.api.nvim_set_hl(0, "WinBar", { fg = color.gray7, bg = color.gray3, bold = true })
         vim.api.nvim_set_hl(0, "WinBarNC", { fg = color.gray7, bg = color.gray3, bold = false })
         vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = color.orange })
+
+        vim.api.nvim_set_hl(0, "ColorColumn", { fg = theme.ui.fg_statusline, bg = theme.ui.bg_statusline })
     end
 }
