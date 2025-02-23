8b47d76e57a8a976a543fef2e5e6753dca007861
pdkvi <pdkvi0@gmail.com>
2025-02-24 01:14:44 +0300

    feat(tabline): add lsp server status section

--- lua/plugins/specs/tabline.lua
+++ lua/plugins/specs/tabline.lua
@@ -1,5 +1,3 @@
--- TODO: change list of windows with LSP status or smth like that
-
 local theme =
 {
     fill = 'TabLineFill',
@@ -21,7 +19,12 @@ end
 return
 {
     "nanozuki/tabby.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies =
+    {
+        "nvim-tree/nvim-web-devicons",
+        "linrongbin16/lsp-progress.nvim"
+    },
+
     config = function()
         vim.o.showtabline = 2
 
@@ -43,6 +46,69 @@ return
 
         vim.keymap.set("n", "<leader>tr", tab_rename)
 
+        local progress = require("lsp-progress")
+        progress.setup({
+            client_format = function(client_name, spinner, series_messages)
+                if #series_messages == 0 then return nil end
+
+                return
+                {
+                    name = client_name,
+                    body = spinner .. " " .. table.concat(series_messages, ", ")
+                }
+            end,
+
+            format = function(client_messages)
+                --- @param name string
+                --- @param msg string?
+                --- @return string
+                local function stringify(name, msg)
+                    return msg and string.format("%s %s", name, msg) or name
+                end
+
+                local sign = "LSP ::"
+
+                local messages_map = {}
+                for _, climsg in ipairs(client_messages) do
+                    messages_map[climsg.name] = climsg.body
+                end
+
+                local clients = vim.iter(vim.lsp.get_clients())
+                clients = clients:filter(function(client)
+                    return type(client) == "table"
+                        and type(client.name) == "string"
+                        and string.len(client.name) > 0
+                end)
+
+                local builder = {}
+                clients:each(function(client)
+                    if messages_map[client.name] then
+                        table.insert(builder, stringify(client.name, messages_map[client.name]))
+                    else
+                        table.insert(builder, stringify(client.name))
+                    end
+                end)
+
+                if #builder > 0 then
+                    return sign .. " " .. table.concat(builder, ", ")
+                end
+
+                return ""
+            end
+        })
+
+        local progress_to_element = function(line)
+            local content = progress.progress()
+            if content == "" then return "" end
+
+            return content == "" and "" or
+            {
+                line.sep(' █', theme.head, theme.fill),
+                { content, hl = theme.head },
+                line.sep('█', theme.head, theme.fill),
+            }
+        end
+
         local tabby = require("tabby")
         tabby.setup({
             line = function(line)
@@ -56,6 +122,7 @@ return
 
                     { "TIME: " .. os.date("%H:%M") },
 
+                    progress_to_element(line),
 
                     line.spacer(),
 
@@ -82,6 +149,14 @@ return
                     hl = theme.fill,
                 }
             end,
+        })
+
+        vim.api.nvim_create_augroup("tabby_augroup", { clear = true })
+        vim.api.nvim_create_autocmd("User",
+        {
+            group = "tabby_augroup",
+            pattern = "LspProgressStatusUpdated",
+            callback = function() tabby.update() end
         })
     end
 }
