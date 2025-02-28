4f348804929d9db3a4129f503596c90af5fae14d
pdkvi <pdkvi0@gmail.com>
2025-02-28 03:46:31 +0300

    feat(keymaps): change keymaps for buffer navigation

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,7 @@
 vim.keymap.set({ "n", "i" }, "<C-s>", function() vim.cmd("w") end)
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>")
 
-vim.keymap.set("n", "gn", "<cmd>bnext<cr>")
-vim.keymap.set("n", "gp", "<cmd>bprevious<cr>")
+vim.keymap.set("n", "gf", "<cmd>bnext<cr>")
+vim.keymap.set("n", "gF", "<cmd>bprevious<cr>")
 
 return false
